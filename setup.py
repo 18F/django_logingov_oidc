423e8bad2bcf023c23611f306821ceec2258c1f3
Tim Ballard <1425377+timoballard@users.noreply.github.com>
2022-12-01 12:50:57 -0600

    add setup.py, update pyproject.toml

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django_logingov_backend',
+    version='0.0.3',
+    description='A Django OpenID Connect authentication backend designed for use with Login.gov',
+    packages=find_packages(),
+)

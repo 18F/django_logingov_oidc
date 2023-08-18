99c58752a156122767a48ed08da38ecece1f57c2
Tim Ballard <timopjotr@gmail.com>
2023-08-18 11:43:34 -0500

    0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django_logingov_backend',
-    version='0.0.3',
+    version='0.0.5',
     description='A Django OpenID Connect authentication backend designed for use with Login.gov',
     packages=find_packages(),
 )
